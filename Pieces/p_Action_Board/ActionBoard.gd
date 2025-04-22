9941d9c090749af9a36066926f6c68ed0786d5f4
GiftedR <kidsgamers@hotmail.com>
2025-04-22 08:40:25 -0700

    Added Full Text Buttons to Acion Boards

--- Pieces/p_Action_Board/ActionBoard.gd
+++ Pieces/p_Action_Board/ActionBoard.gd
@@ -1,8 +1,15 @@
 extends CanvasLayer
 class_name ActionBoard
 
-@onready var btn2_Cancel:Button = $ctr2_Action_Board/mgc2_Actions/VBoxContainer/hbc2_Window_Controls/btn2_Cancel
-@onready var btn2_Confirm:Button = $ctr2_Action_Board/mgc2_Actions/VBoxContainer/hbc2_Window_Controls/btn2_Confirm
+@onready var btn2_Cancel:Button = $ctr2_Action_Board/mgc2_Actions/vbc2_Actions/hbc2_Window_Controls/btn2_Cancel
+@onready var btn2_Confirm:Button = $ctr2_Action_Board/mgc2_Actions/vbc2_Actions/hbc2_Window_Controls/btn2_Confirm
+
+@onready var btn2_full_text:Button = $ctr2_Action_Board/mgc2_Actions/vbc2_Actions/hbc2_Window_Controls/btn2_Full_Text
+
+@onready var action_panel:PanelContainer = $ctr2_Action_Board/mgc2_Actions/vbc2_Actions/plc2_Actions
+@onready var text_panel:PanelContainer = $ctr2_Action_Board/mgc2_Actions/vbc2_Actions/plc2_Full_Text
+
+var is_full_text_visible:bool = false
 
 static func spawn() -> ActionBoard:
 	var board:ActionBoard = load("res://Pieces/p_Action_Board.tscn").instantiate()
@@ -12,6 +19,13 @@ static func spawn() -> ActionBoard:
 func _ready() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 	btn2_Cancel.pressed.connect(func() -> void: self.queue_free())
+	btn2_full_text.pressed.connect(func() -> void:
+		is_full_text_visible = !is_full_text_visible
+
+		action_panel.visible = !is_full_text_visible
+		text_panel.visible = is_full_text_visible
+	)
+
 
 func _enter_tree() -> void:
 	Player.instance.with_busy()
